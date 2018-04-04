9a9f8af64dcd00d29827fe1e490e22e748fb935f
Peter Rhodes <rhodespeter@hotmail.co.uk>
2018-04-04 21:56:55 +0100

    Add blue colour to text in console

--- js/index.js
+++ js/index.js
@@ -6,17 +6,23 @@ import hasWon from './modules/has-won';
 import logTotal from './modules/log-total';
 import { dealCards, dealOneCard } from './modules/deal-cards';
 
+const consoleColourBlue = '\x1b[34m\x1b';
+const escapeConsoleColour = '\x1b[0m';
 let makeOneMove;
+let round = 0;
 
 const gameLoop = (cardsInPlay, gameDeck) => {
   const sam = totaliser(cardsInPlay.sam);
   const dealer = totaliser(cardsInPlay.dealer);
   const playerWon = hasWon(sam, dealer);
+  round += 1;
 
+  console.log(consoleColourBlue, `Round ${round}:`, escapeConsoleColour);
   console.log(logPlayerCards(cardsInPlay));
   console.log(logTotal(sam, dealer));
 
   if (playerWon) {
+    console.log(consoleColourBlue, 'Game over!', escapeConsoleColour);
     console.log(playerWon);
   } else if (sam < 17) {
     makeOneMove('sam', cardsInPlay, gameDeck);
@@ -36,7 +42,7 @@ const startGame = () => {
   const gameDeck = shuffleDeck(deck);
   const cardsInPlay = dealCards(gameDeck);
 
-  console.log('Game started \n');
+  console.log(consoleColourBlue, 'Game started \n', escapeConsoleColour);
   gameLoop(cardsInPlay, gameDeck);
 };
 
